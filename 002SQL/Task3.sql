699404af7ab4c081a86d5e3a38a1885a54df46ba
Barash Anton <36210606+Anton3737@users.noreply.github.com>
2022-08-15 15:10:35 +0300

    Add files via upload

---
+++ 002SQL/Task3.sql
@@ -0,0 +1,56 @@
+
+
+
+https://www.w3schools.com  та w3schools.com   -- різні сайти  
+
+
+https://www.w3schools.com/sql/sql_select.asp
+SELECT
+
+-- Команда SELECT отримання даних із таблиці
+
+
+https://www.w3schools.com/sql/sql_unique.asp
+UNIQUE
+
+-- Команда вказує що всі поля в в колонці є унікальними
+
+
+https://www.w3schools.com/sql/sql_groupby.asp
+GROUP BY
+
+-- Команда GROUP BY  групування вихідних даних
+
+
+https://www.w3schools.com/sql/sql_insert.asp
+INSERT INTO
+
+-- Команда INSERT INTO  надає нам функціонал додавання даних в новостворену таблицю
+
+
+https://www.w3schools.com/sql/sql_where.asp
+WHERE
+
+
+https://www.w3schools.com/sql/sql_create_db.asp
+CREATE DATABASE
+-- Команда CREATE DATABASE - створює нову Базу даниз 
+
+
+https://www.w3schools.com/sql/sql_drop_table.asp
+DROP TABLE
+
+-- Команда DROP TABLE - вичищає таблицю оркім її полів в першочерговий її вигляд
+
+
+
+https://www.w3schools.com/mysql/mysql_primarykey.asp
+PRIMARY KEY 
+
+-- Команда PRIMARY KEY гарантує унікальність ідентицікатора
+
+
+https://www.w3schools.com/mysql/mysql_foreignkey.asp
+FOREIGN KEY
+
+-- Команда FOREIGN KEY зовнішій ключ який гарантує зв'язку таблиць між собою
